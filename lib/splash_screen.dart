38bccfade3b81b12977a8d51ea259e97d5e2b228
mahmoudelnagar <mahmoudelnagarx1@gmail.com>
2025-03-08 03:01:01 +0200

    update

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,11 +1,9 @@
 import 'dart:async';
-
 import 'package:education_app/screens/login.dart';
 import 'package:flutter/material.dart';
 
-
 class SplashScreen extends StatefulWidget {
-  static const String routeName="routeName";
+  static const String routeName = "routeName";
 
   @override
   _SplashScreenState createState() => _SplashScreenState();
@@ -15,7 +13,6 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    // Navigate to the main screen after a delay
     Timer(Duration(seconds: 5), () {
       Navigator.pushReplacement(
         context,
@@ -27,18 +24,10 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-
-                height: 821,
-                width: double.infinity,
-                child: Image.asset(
-                  'assets/icons/splash.jpg', fit: BoxFit.fill,
-                )),
-          ],
+      body: SizedBox.expand(  // Ensures full-screen coverage
+        child: Image.asset(
+          'assets/icons/splash.jpg',
+          fit: BoxFit.cover,  // Ensures the image covers the screen
         ),
       ),
     );
